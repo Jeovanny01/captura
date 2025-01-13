76843f5e92528b74987e0cb26d74a534e1785bc1
jeovanny_campos@hotmail.com <jeovanny_campos@grupocarosa.com>
2025-01-13 17:38:56 -0600

    Primer commit: Proyecto inicial

--- acciones.js
+++ acciones.js
@@ -8,10 +8,11 @@ function showSection(sectionId) {
 document.addEventListener('DOMContentLoaded', () => {
     showSection('register');
 });
-
 function iniciarEscaneo() {
+    const cameraContainer = document.getElementById("camera-container");
+
     // Mostrar el contenedor de la cámara
-    document.getElementById("camera-container").style.display = "block";
+    cameraContainer.style.display = "block";
 
     Quagga.init({
         inputStream: {
@@ -24,7 +25,7 @@ function iniciarEscaneo() {
         decoder: {
             readers: ["code_128_reader", "ean_reader", "ean_8_reader"] // Tipos de códigos de barras
         }
-    }, function(err) {
+    }, function (err) {
         if (err) {
             console.error("Error al iniciar Quagga:", err);
             detenerEscaneo();
@@ -35,8 +36,8 @@ function iniciarEscaneo() {
     });
 
     // Escuchar los resultados de escaneo
-    Quagga.onDetected(function(result) {
-        if (result && result.codeResult && result.codeResult.code) {
+    Quagga.onDetected(function (result) {
+        if (result?.codeResult?.code) {
             console.log("Código detectado:", result.codeResult.code);
             document.getElementById("codigo").value = result.codeResult.code; // Poner el código en el input
             detenerEscaneo(); // Detener el escaneo después de capturar un código
@@ -45,7 +46,6 @@ function iniciarEscaneo() {
 }
 
 function detenerEscaneo() {
-    // Detener Quagga y ocultar el contenedor de la cámara
-    Quagga.stop();
-    document.getElementById("camera-container").style.display = "none";
+    Quagga.stop(); // Detener Quagga
+    document.getElementById("camera-container").style.display = "none"; // Ocultar la cámara
 }
