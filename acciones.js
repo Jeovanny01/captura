90040512bbe6b97a3fbeefeddd1892861218e11d
jeovannycampos <jeovanny_campos@grupocarosa.com>
2025-05-26 08:15:12 -0600

    problemas al seleccionar precios de mmag

--- acciones.js
+++ acciones.js
@@ -579,7 +579,7 @@ function actualizarCampos4(prod) {
     document.getElementById("descripcion4").value = prod.DESCRIPCION || '';
     document.getElementById("item4").value = prod.ITEM || '';
 
- if (empresa =="MMAG" && typeof productoCodigo !== "undefined" && productoCodigo.length > 0)
+ if (empresa.trim()  ==="MMAG"  && productoCodigo)
  {    document.getElementById("precio4").value = productoCodigo.PRECIO  || prod.PRECIO_MAYOREO || prod.PRECIO ||  0; } else
  {  document.getElementById("precio4").value =  prod.PRECIO_MAYOREO || prod.PRECIO ||  0;}
 
